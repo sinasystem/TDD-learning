e1d3ac2cf3949c34ec11bfcb6f94dfa248b0fc1a
Sina Nourossana <sina.nourossana@gmail.com>
2021-12-02 05:57:11 +0330

    added dollar to test names in order to add more currencies

--- src/test/java/MoneyTest.java
+++ src/test/java/MoneyTest.java
@@ -4,19 +4,20 @@ import static org.junit.jupiter.api.Assertions.*;
 
 public class MoneyTest {
     @Test
-    void testAmount(){
+    void testAmountDollar(){
         Dollar five = new Dollar(5);
         assertEquals(5,five.amount);
     }
     @Test
-    void testMultiplication(){
+    void testMultiplicationDollar(){
         Dollar five = new Dollar(5);
         five.times(2);
         assertEquals(10,five.amount);
     }
     @Test
-    void testEqual(){
+    void testEqualDollar(){
         assertEquals(new Dollar(5),new Dollar(5));
         assertNotEquals(new Dollar(5),new Dollar(10));
     }
+
 }
