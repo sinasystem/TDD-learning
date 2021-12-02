b2431c3e36a598fd41f97acbaa6091e28cd60cf8
Sina Nourossana <sina.nourossana@gmail.com>
2021-12-02 06:00:08 +0330

    Wrote some basic tests for franc

--- src/test/java/MoneyTest.java
+++ src/test/java/MoneyTest.java
@@ -19,5 +19,20 @@ public class MoneyTest {
         assertEquals(new Dollar(5),new Dollar(5));
         assertNotEquals(new Dollar(5),new Dollar(10));
     }
-
+    @Test
+    void testAmountFranc(){
+        Franc five = new Franc(5);
+        assertEquals(5,five.amount);
+    }
+    @Test
+    void testMultiplicationFranc(){
+        Franc five = new Franc(5);
+        five.times(2);
+        assertEquals(10,five.amount);
+    }
+    @Test
+    void testEqualFranc(){
+        assertEquals(new Franc(5),new Franc(5));
+        assertNotEquals(new Franc(5),new Franc(10));
+    }
 }
