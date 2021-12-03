d7ef4ff3ea37f8a04bd3f440c563ac56e98ac49a
Sina Nourossana <sina.nourossana@gmail.com>
2021-12-03 15:17:06 +0330

    Refactored tests

--- src/test/java/tdd/money/MoneyTest.java
+++ src/test/java/tdd/money/MoneyTest.java
@@ -6,42 +6,34 @@ import static org.junit.jupiter.api.Assertions.*;
 
 public class MoneyTest {
     @Test
-    void testAmountDollar(){
-        Money five = Money.dollar(5);
-        assertEquals(5,five.amount);
+    void testAmount(){
+        Money fiveDollar = Money.dollar(5);
+        assertEquals(5,fiveDollar.amount);
+
+        Money fiveFranc = Money.franc(5);
+        assertEquals(5,fiveFranc.amount);
     }
     @Test
-    void testMultiplicationDollar(){
-        Money five = Money.dollar(5);
-        assertEquals(Money.dollar(10),five.times(2));
-        assertEquals(Money.dollar(15),five.times(3));
+    void testMultiplication(){
+        Money fiveDollar = Money.dollar(5);
+        assertEquals(Money.dollar(10),fiveDollar.times(2));
+        assertEquals(Money.dollar(15),fiveDollar.times(3));
+
+        Money fiveFranc = Money.franc(5);
+        assertEquals(Money.franc(10),fiveFranc.times(2));
+        assertEquals(Money.franc(15),fiveFranc.times(3));
     }
     @Test
-    void testEqualityDollar(){
+    void testEquality(){
         assertEquals(Money.dollar(5),Money.dollar(5));
         assertNotEquals(Money.dollar(5),Money.dollar(10));
 
-    }
-    @Test
-    void testAmountFranc(){
-        Money five = Money.franc(5);
-        assertEquals(5,five.amount);
-    }
-    @Test
-    void testMultiplicationFranc(){
-        Money five = Money.franc(5);
-        assertEquals(Money.franc(10),five.times(2));
-        assertEquals(Money.franc(15),five.times(3));
-    }
-    @Test
-    void testEqualityFranc(){
         assertEquals(Money.franc(5),Money.franc(5));
         assertNotEquals(Money.franc(5),Money.franc(10));
-    }
-    @Test
-    void testEqualityDollarFranc(){
+
         assertNotEquals(Money.franc(5),Money.dollar(5));
-     }
+
+    }
     @Test
     void testCurrency(){
         assertEquals("USD",Money.dollar(1).currency());
