96b8f5d5ae41417feb35c02ad6014dcb9da36d21
Sina Nourossana <sina.nourossana@gmail.com>
2021-12-07 21:38:28 +0330

    Implemented functionality of rates

--- src/main/java/tdd/money/Bank.java
+++ src/main/java/tdd/money/Bank.java
@@ -1,13 +1,20 @@
 package tdd.money;
 
+import java.util.HashMap;
+
 public class Bank {
+    private HashMap<Pair,Integer> rateMap = new HashMap<>();
     public Money reduce(Expression source,String toCurrency){
         return source.reduce(this, toCurrency);
     }
     public int rate(String from, String to){
-        return (from.equals("CHF") && to.equals("USD")) ? 2 : 1;
+      if(from.equals(to)){
+        return 1;
+      }
+      return rateMap.get(new Pair(from,to));
     }
 
     public void addRate(String from, String to, int rate) {
+        rateMap.put(new Pair(from,to),rate);
     }
 }
--- src/main/java/tdd/money/Money.java
+++ src/main/java/tdd/money/Money.java
@@ -28,8 +28,7 @@ public class Money implements Expression{
     }
     @Override
     public Money reduce(Bank bank,String to){
-//        return this;
-        int rate = (currency.equals("CHF") && to.equals("USD")) ? 2 : 1;
+        int rate = bank.rate(currency,to);
         return new Money(amount/rate,to);
     }
     @Override
