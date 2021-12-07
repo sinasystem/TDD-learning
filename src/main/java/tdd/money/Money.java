9b0bb6da394d5f1b05b7c33ddb5a13dc54276912
Sina Nourossana <sina.nourossana@gmail.com>
2021-12-07 19:06:09 +0330

    Implemented expression into money object added plus expression

--- src/main/java/tdd/money/Money.java
+++ src/main/java/tdd/money/Money.java
@@ -2,7 +2,7 @@ package tdd.money;
 
 import java.util.Objects;
 
-public class Money {
+public class Money implements Expression{
 
     protected int amount;
     protected String currency;
@@ -23,7 +23,13 @@ public class Money {
     public Money times(int multiplier){
         return new Money(amount*multiplier,this.currency);
     }
-
+    public Expression plus(Money addend){
+        return new Sum(this,addend);
+    }
+    @Override
+    public Money reduce(String to){
+        return this;
+    }
     @Override
     public boolean equals(Object object) {
         Money money = (Money) object;
